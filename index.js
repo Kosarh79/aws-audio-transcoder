420b2009c2781a08a0a2d47c4d9c4e031ccb9814
Kosar Hosseinkhani <kosarh79@gmail.com>
2016-12-12 11:10:37 -0800

    Invoke notifySonicspan lambda function

---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+ * Created by K2 on 12/12/2016.
+ */
+// Library Code
+//At this point only mp4 video is created.
+var aws = require('aws-sdk');
+var elastictranscoder = new aws.ElasticTranscoder();
+var path = require('path');
+// return basename without extension
+function basename(path) {
+    return path.split('/').reverse()[0].split('.')[0];
+}
+
+// return output file name with timestamp and extension
+function outputKey(name, ext) {
+    return name + '_trans.' + ext ;
+}
+function notifySonicspan(filename, context){
+    var lambda = new aws.Lambda({
+        region: process.env.region
+    });
+    console.log('....notifying sonicspan from imgresize');
+    var params = {
+        FunctionName: process.env.functionName,
+        Payload: JSON.stringify({"data": filename})
+    };
+    lambda.invoke(params, function(error, data) {
+        if (error) {
+            console.log('Error on notifying... ' + error);
+            context.done('error', error);
+        }
+        if(data.Payload){
+            context.succeed(data.Payload)
+        }
+    });
+}
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    // Get the object from the event and show its content type
+    var key = event.Records[0].s3.object.key;
+    var filename = path.basename(key);
+    var params = {
+        Input: {
+            Key: key
+        },
+        PipelineId: '1479569182711-766cpt',
+        // OutputKeyPrefix: 'transecoded/',
+        OutputKeyPrefix: 'transcodedaudios/',
+        Outputs: [
+            {
+                Key: outputKey(basename(key),'mp3'),
+                PresetId: '1351620000001-300040', /*Audio MP3 - 128 kilobits/second*/
+            }
+        ]
+    };
+
+    elastictranscoder.createJob(params, function(err, data) {
+        console.log('Converting the Audio is done for ' + filename);
+        if (err){
+            console.log(err, err.stack); // an error occurred
+            context.fail();
+            return;
+        }
+        /** Notify Sonicspan */
+        notifySonicspan(filename, context);
+        //context.succeed();
+    });
+};
